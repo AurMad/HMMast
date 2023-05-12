8a42bb37e404dec521b6df7b59ef1695364838de
Aurélien Madouasse <amadouasse@BIOEPAR.LOCAL>
2023-05-12 17:01:30 +0200

    Modification of the file used to test the model including continuous and categorical test results.

--- notebooks/analysis_simulated_data.R
+++ notebooks/analysis_simulated_data.R
@@ -12,7 +12,7 @@ trans_mat[2, ] <- c(.1, .9, 0)
 trans_mat[3, ] <- c(1, 0, 0)
 
 ## Simulating data
-mast <- sim_mast(n_cows = 10,
+mast <- sim_mast(n_cows = 100,
          n_times = 12,
          n_states = 3,
          trans_mat = trans_mat)
@@ -29,4 +29,34 @@ mast <- mast |>
 ## other transitions defined in relation to these
 ## with normal distributions on the log scale
 ## each lactation is considered as a unit
-md0 <- hmm_3s_2t_f(mast, level = "cow")
+md0 <- hmm_3s_2t_f(mast, level = "cow",
+                   chains = 2,
+                   iter_sampling = 500)
+
+## parameter summary
+## test_char[1] is the probability of a false positive
+## B[,] is the transition matrix on the probability scale
+View(md0$summary)
+
+## MCMC draws
+draws <- md0$draws
+
+## plotting the transition matrix
+transition_labels <- matrix(
+  c("H to H", "H to C", "H to E",
+    "C to H", "C to C", "C to E",
+    "E to H", "E to C", "E to E"),
+  ncol = 3, byrow = TRUE)
+par(mfrow = c(3, 3))
+for(i in 1:3){
+
+  ii <- grep(paste0("B\\[", i), colnames(draws))
+
+  for(j in 1:3){
+
+    hist(draws[,ii[j]], breaks = 100, xlim = c(0, 1),
+         main = transition_labels[i,j])
+
+  }
+}
+
